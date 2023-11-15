c9a577c944b74351915a6807717c94e1e9f654f1
jiwonMe <park@jiwon.me>
2023-11-15 19:33:05 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import View from './components/ui/View'
+
+function App() {
+  return (
+    <>
+      <View />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ui/View.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const View = () => {
+  return (
+    <div>View</div>
+  )
+}
+
+export default View
+
+const ViewLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
